8b4ad3eb8808490fde154d52a047942b23acb867
AleksandrSpiridonov <AleksandrSpiridonov@users.noreply.github.com>
2026-09-24 12:55:18 +0000

    deploy: e24e66a0433006e8930eaece17e73137fec3576c

---
+++ postscript-757c4289.js
@@ -0,0 +1,14 @@
+await Promise.all([
+  import("./static/scripts/script-0-f7c52b6a.js"),
+  import("./static/scripts/script-1-ec880d15.js"),
+  import("./static/scripts/script-2-1f9394e6.js"),
+  import("./static/scripts/script-3-2a102aaa.js"),
+  import("./static/scripts/script-4-cc8afa9d.js"),
+  import("./static/scripts/script-5-7e107d7e.js"),
+  import("./static/scripts/script-6-5ef560a8.js"),
+  import("./static/scripts/script-7-f960c885.js"),
+  import("./static/scripts/script-8-632b2822.js"),
+  import("./static/scripts/script-9-be15df42.js"),
+  import("./static/scripts/script-10-e37aea36.js")
+]);
+await import("./static/scripts/script-11-d93af9ba.js");
